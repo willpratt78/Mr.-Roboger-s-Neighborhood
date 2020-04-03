84a6fcf0eae85601ac9b4f92ed6f88c2e7d4b63e
William Pratt <willpratt78@gmail.com>
2020-04-03 09:21:42 -0700

    creat if statment defining if it's a number or not

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
 // business logic
 
-var response =[[1='Beep!'], [2='Boop!'], [3="won't you be my neighbor?"]]
+var responseSet = [[1, 'Beep!'], [2, 'Boop!'], [3, "won't you be my neighbor?"]];
 
+var numChecker = function(number) {
+  if (number === parseInt(number)) {
+    return true; 
+  } else {
+    return false;
+  }
+}
+
+var response = function(number) {
+  for (var i =0; i< responseSet.length; i ++) {
+    if (number.includes(3))
+    return [i][1];
+  }
+
+}
 
 
 
@@ -9,7 +24,13 @@ var response =[[1='Beep!'], [2='Boop!'], [3="won't you be my neighbor?"]]
 $(document).ready(function() {
   $("#converter").submit(function(event) {
     event.preventDefault();
-    var result = $("#number").val();
+    var userInput = parseInt($("#number").val());
+
+    if (numChecker(userInput) === true){
+      result = 'hello' //response(userInput)
+    } else {
+      result = "Beep, boop, This isn't a number try again"
+    }
     
     
     $('#output').text(result);
